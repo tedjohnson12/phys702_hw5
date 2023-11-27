4147b37c2bfd82cde902d73dde549055fe96c2ff
Ted Johnson <ted.johnson@unlv.edu>
2023-11-26 20:55:53 -0800

    clean and add plotting

--- scripts/solve.py
+++ scripts/solve.py
@@ -1,6 +1,6 @@
 
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 from phys702hw5 import solver
 
@@ -9,25 +9,82 @@ from math import pi
 
 bounds = (0.0, pi/2.0)
 
-y_init = 0
+YINIT = 0
 
-x_final = 3
+XFINAL = 3
 
 NITER = 10
-
+STEP_SIZE = 0.01
+FMT = '.4f'
 
 def get_xend(theta)->float:
-    x,_,_ = solver.solve_python(y_init,theta,0.01)
+    """
+    Get the final x value for a given angle.
+    
+    Parameters
+    ----------
+    theta : float
+        The angle in radians.
+    
+    Returns
+    -------
+    float
+        The final x value.
+    """
+    x,_,_ = solver.solve_python(YINIT,theta,STEP_SIZE)
     return x[-1]
 
 def get_next_guess(theta_low,theta_high):
+    """
+    Get next guess.
+    
+    Parameters
+    ----------
+    theta_low : float
+        The lower bound on theta.
+    theta_high : float
+        The upper bound on theta.
+    
+    Returns
+    -------
+    float
+        The next theta to guess
+    
+    """
     return (theta_low+theta_high)/2
 
-
 def is_too_high(xfinal:float):
-    return xfinal > x_final
+    """
+    If the final x value is too high.
+    
+    Parameters
+    ----------
+    xfinal : float
+        The final x value.
+    
+    Returns
+    -------
+    bool
+        If the final x value is too high.
+    """
+    return xfinal > XFINAL
 
 def get_new_bounds(theta_low,theta_high):
+    """
+    Update the bounds.
+    
+    Parameters
+    ----------
+    theta_low : float
+        The lower bound on theta.
+    theta_high : float
+        The upper bound on theta.
+    
+    Returns
+    -------
+    tuple
+        The new bounds.
+    """
     theta = get_next_guess(theta_low,theta_high)
     xfinal = get_xend(theta)
     if is_too_high(xfinal):
@@ -36,10 +93,25 @@ def get_new_bounds(theta_low,theta_high):
         theta_high = theta
     return (theta_low,theta_high)
 
-for _ in range(NITER):
-    (theta_low,theta_high) = get_new_bounds(*bounds)
-    bounds = get_new_bounds(theta_low,theta_high)
-
-print(f'Theta is between {bounds[0]*180/np.pi} and {bounds[1]*180/np.pi} degrees')
-
-0
+if __name__ in ['__main__']:
+    for _ in range(NITER):
+        (theta1,theta2) = get_new_bounds(*bounds)
+        bounds = get_new_bounds(theta1,theta2)
+    
+    print(f'Theta is between {bounds[0]*180/np.pi:{FMT}} and {bounds[1]*180/np.pi:{FMT}} degrees')
+    
+    theta = get_next_guess(*bounds)
+    
+    x,y,z = solver.solve_python(YINIT,theta,STEP_SIZE)
+    
+    plt.plot(x,y,label='\\phi (rad)')
+    plt.plot(x,z,label='\\omega (rad/s)')
+    
+    plt.axvline(x=XFINAL,color='k',linestyle='dashed')
+    plt.axhline(y=pi/2,color='k',linestyle='dashed')
+    
+    plt.xlabel('time (s)')
+    plt.title(f'\\theta = {theta*180/np.pi:{FMT}} degrees')
+    
+    plt.savefig('plot.png')
+    
